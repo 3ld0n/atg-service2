785fdd8b13e00610ee1fd1ce4c6b404197e947b6
djlogan <djlogan2.dl@gmail.com>
2015-08-25 10:12:32 -0600

    Made each service depend upon each other

--- service2.js
+++ service2.js
@@ -6,17 +6,75 @@ var express = require('express')
     ,_     = require('underscore');
 var app = express();
 
-http.createServer(app).listen(8080, function(){
-    console.log('Express server listening on port ' + 8080);
+http.createServer(app).listen(8888, function(){
+    console.log('Express server listening on port ' + 8888);
 });
 
-app.get('/service2/:text', function(req, rsp){
+app.get('/service2/one/:text', function(req, rsp){
     "use strict";
     var r = {};
-    r.words = req.params.text.split(' ');
-    r.shortest = _.reduce(r.words, function(len,word){return word.length < len ? word.length : len},req.params.text.length);
-    r.longest = _.reduce(r.words, function(len,word){return word.length > len ? word.length : len},0);
-    r.first = r.words[0];
-    r.last = _.last(r.words);
+    //r.words = req.params.text.split(' ');
+    var words = req.params.text.split(' ');
+    r.shortest = _.reduce(words, function(len,word){return word.length < len ? word.length : len},req.params.text.length);
+    r.longest = _.reduce(words, function(len,word){return word.length > len ? word.length : len},0);
+    //r.first = r.words[0];
+    //r.last = _.last(r.words);
+    rsp.type('json');
     rsp.json(r);
+});
+
+app.get('/service2/all/:text', function(req, rsp){
+    "use strict";
+    var service1 = {
+        host: "localhost",
+        port: 8887,
+        path: '/service1/one/' + encodeURIComponent(req.params.text),
+        method: 'GET'
+    };
+    var service3 = {
+        host: "localhost",
+        port: 8889,
+        path: '/service3/one/' + encodeURIComponent(req.params.text),
+        method: 'GET'
+    };
+
+    var services = 0;
+    var r = {};
+
+    var words = req.params.text.split(' ');
+    r.shortest = _.reduce(words, function(len,word){return word.length < len ? word.length : len},req.params.text.length);
+    r.longest = _.reduce(words, function(len,word){return word.length > len ? word.length : len},0);
+
+    rsp.type('json');
+
+    var do_data = function(chunk) {
+        _.extend(r, JSON.parse(chunk));
+        if(++services == 2)
+            rsp.json(r);
+    };
+    var do_error = function(serv) {
+        if(!r.unavailable)
+            r.unavailable = [serv];
+        else
+            r.unavailable.push(serv);
+        if(++services == 2)
+            rsp.json(r);
+    };
+
+    var req1 = http.request(service1, function(res) {
+        res.setEncoding('utf8');
+        res.on('data', do_data);
+    });
+
+    var req3 = http.request(service3, function(res) {
+        res.setEncoding('utf8');
+        res.on('data', do_data);
+    });
+
+    req1.on('error', function(e){do_error("service1")});
+    req3.on('error', function(e){do_error("service3")});
+
+    req1.end();
+    req3.end();
+
 });
